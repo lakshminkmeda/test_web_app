1a90f0f872d6304726f8d3101172587b2a69f1dd
lakshminkmeda <lakshmi.nareen@gmail.com>
2024-03-18 11:10:39 +0000

    V3 - Added authenticate

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Temporary website files for the project "Build a Serverless Web Application"
-# https://aws.amazon.com/getting-started/hands-on/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/
-
-Temporary website files for the project
+# web_app
+Web app
---
+++ authenticate.js
@@ -0,0 +1,30 @@
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
